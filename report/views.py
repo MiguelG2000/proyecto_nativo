a529aadf1c394264a71f7bb16497daf88f660a97
MiguelG2000 <mikegonzalez@MacBook-Air-de-Miguel.local>
2025-03-02 22:57:49 -0600

    Primer intento de graficas

--- report/views.py
+++ report/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from django.db.models import Sum
+from cotizaciones.models import Cotizaciones, CotizacionProduct
+from products.models import Product
 
 # Create your views here.
 def report_dashboard(request):
-    return render(request, 'report/dashboard.html')
+    grafica = ( CotizacionProduct.objects
+                .filter(cotizacion_id__status="Aceptado")
+                .values('product_id__nombre')
+                .annotate(total_vendido=Sum('cantidad'))
+                .order_by('-total_vendido')[:10]
+                )
+
+    labels = [producto['product_id__nombre'] for producto in grafica]
+    data = [producto["total_vendido"] for producto in grafica]
+
+    context = {
+        'labels': labels,
+        'data': data
+    }
+
+    print(str(grafica))
+    return render(request, 'report/dashboard.html', context)
